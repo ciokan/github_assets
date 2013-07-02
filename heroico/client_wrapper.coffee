5a1fbe726ad6591499c2d8ca8b4df3e54426c83f
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2013-07-02 23:07:30 +0300

    assets

--- heroico/client_wrapper.coffee
+++ heroico/client_wrapper.coffee
@@ -7,7 +7,37 @@ class Heroico
 		@wrapper_div = $('<div></div>').attr("id", "hr_wrapper").appendTo("body")
 	
 	add_chat_frame: ->
+		@chat_frame = $('<iframe></iframe>').attr({
+			src: "http://localhost:4000/"+user_id,
+			style: "border: none; \
+					background: transparent;\
+					margin: 0; \
+					padding: 0; \
+					position: absolute; \
+					bottom: 0; \
+					right: 20px; \
+					height: 450px; \
+					width: 350px"
+		}).appendTo(@wrapper_div)
 
+load_jquery = (cb)->
+	# Load the script
+	script = document.createElement("SCRIPT")
+	script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"
+	script.type = "text/javascript"
+	document.getElementsByTagName("head")[0].appendChild script
+  
+	# Poll for jQuery to come into existance
+	checkReady = (callback) ->
+		if window.jQuery
+			callback jQuery
+		else
+			window.setTimeout (->
+				checkReady callback
+			), 100
 
-window.jQuery || document.write('<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js">\x3C/script>')
-hr = new Heroico()
+	checkReady ($) -> cb()
+
+window.jQuery || load_jquery(->
+	hr = new Heroico()
+)
--- heroico/client_wrapper.js
+++ heroico/client_wrapper.js
@@ -1,5 +1,5 @@
 (function() {
-  var Heroico, hr;
+  var Heroico, load_jquery;
 
   Heroico = (function() {
 
@@ -12,14 +12,40 @@
       return this.wrapper_div = $('<div></div>').attr("id", "hr_wrapper").appendTo("body");
     };
 
-    Heroico.prototype.add_chat_frame = function() {};
+    Heroico.prototype.add_chat_frame = function() {
+      return this.chat_frame = $('<iframe></iframe>').attr({
+        src: "http://localhost:4000/" + user_id,
+        style: "border: none; \					background: transparent;\					margin: 0; \					padding: 0; \					position: absolute; \					bottom: 0; \					right: 20px; \					height: 450px; \					width: 350px"
+      }).appendTo(this.wrapper_div);
+    };
 
     return Heroico;
 
   })();
 
-  window.jQuery || document.write('<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js">\x3C/script>');
+  load_jquery = function(cb) {
+    var checkReady, script;
+    script = document.createElement("SCRIPT");
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js";
+    script.type = "text/javascript";
+    document.getElementsByTagName("head")[0].appendChild(script);
+    checkReady = function(callback) {
+      if (window.jQuery) {
+        return callback(jQuery);
+      } else {
+        return window.setTimeout((function() {
+          return checkReady(callback);
+        }), 100);
+      }
+    };
+    return checkReady(function($) {
+      return cb();
+    });
+  };
 
-  hr = new Heroico();
+  window.jQuery || load_jquery(function() {
+    var hr;
+    return hr = new Heroico();
+  });
 
 }).call(this);
